78580422030203c9ba4215216795896219d0154e
Wayne Meissner <wmeissner@gmail.com>
2013-04-26 13:44:24 +1000

    Commonalize more houdini related code

--- ext/walters.cpp
+++ ext/walters.cpp
@@ -19,88 +19,87 @@ walters_read_cstring(RubyEnv *rb, void *ptr)
 {
     return (const char *) ptr;
 }
-
-static inline void* 
-cstring(gh_buf* buf, bool copy) 
+static inline void*
+houdini(int (*fn)(gh_buf*, const uint8_t *, size_t size), const char* cstr, unsigned int size)
 {
-    if (!copy) {
-        gh_buf_free(buf);
+    gh_buf gh = GH_BUF_INIT;
+    
+    if (!(*fn)(&gh, (const uint8_t *) cstr, size)) {
+        gh_buf_free(&gh);
         return NULL;
     }
 
-    if (gh_buf_oom(buf)) {
+    if (gh_buf_oom(&gh)) {
         throw xni::runtime_error("out of memory");
     }
 
-    return gh_buf_detach(buf);
+    return gh_buf_detach(&gh);
 }
 
 XNI_EXPORT void *
 walters__escape_html(RubyEnv *rb, const char *cstr, unsigned int size, bool secure)
 {
     gh_buf gh = GH_BUF_INIT;
-    return cstring(&gh, houdini_escape_html0(&gh, (const uint8_t *) cstr, size, secure));
+    
+    if (houdini_escape_html0(&gh, (const uint8_t *) cstr, size, secure)) {
+        return gh_buf_detach(&gh);
+    }
+
+    gh_buf_free(&gh);
+
+    return NULL;
 }
 
 XNI_EXPORT void *
 walters__unescape_html(RubyEnv *rb, const char *cstr, unsigned int size)
 {
-    gh_buf gh = GH_BUF_INIT;
-    return cstring(&gh, houdini_unescape_html(&gh, (const uint8_t *) cstr, size));
+    return houdini(houdini_unescape_html, cstr, size);
 }
 
 XNI_EXPORT void *
 walters__escape_xml(RubyEnv *rb, const char *cstr, unsigned int size)
 {
-    gh_buf gh = GH_BUF_INIT;
-    return cstring(&gh, houdini_escape_xml(&gh, (const uint8_t *) cstr, size));
+    return houdini(houdini_escape_xml, cstr, size);
 }
 
 XNI_EXPORT void *
 walters__escape_uri(RubyEnv *rb, const char *cstr, unsigned int size)
 {
-    gh_buf gh = GH_BUF_INIT;
-    return cstring(&gh, houdini_escape_uri(&gh, (const uint8_t *) cstr, size));
+    return houdini(houdini_escape_uri, cstr, size);
 }
 
 XNI_EXPORT void *
 walters__escape_url(RubyEnv *rb, const char *cstr, unsigned int size)
 {
-    gh_buf gh = GH_BUF_INIT;
-    return cstring(&gh, houdini_escape_url(&gh, (const uint8_t *) cstr, size));
+    return houdini(houdini_escape_url, cstr, size);
 }
 
 XNI_EXPORT void *
 walters__escape_href(RubyEnv *rb, const char *cstr, unsigned int size)
 {
-    gh_buf gh = GH_BUF_INIT;
-    return cstring(&gh, houdini_escape_href(&gh, (const uint8_t *) cstr, size));
+    return houdini(houdini_escape_href, cstr, size);
 }
 
 XNI_EXPORT void *
 walters__unescape_uri(RubyEnv *rb, const char *cstr, unsigned int size)
 {
-    gh_buf gh = GH_BUF_INIT;
-    return cstring(&gh, houdini_unescape_uri(&gh, (const uint8_t *) cstr, size));
+    return houdini(houdini_unescape_uri, cstr, size);
 }
 
 XNI_EXPORT void *
 walters__unescape_url(RubyEnv *rb, const char *cstr, unsigned int size)
 {
-    gh_buf gh = GH_BUF_INIT;
-    return cstring(&gh, houdini_unescape_url(&gh, (const uint8_t *) cstr, size));
+    return houdini(houdini_unescape_url, cstr, size);
 }
 
 XNI_EXPORT void *
 walters__escape_js(RubyEnv *rb, const char *cstr, unsigned int size)
 {
-    gh_buf gh = GH_BUF_INIT;
-    return cstring(&gh, houdini_escape_js(&gh, (const uint8_t *) cstr, size));
+    return houdini(houdini_escape_js, cstr, size);
 }
 
 XNI_EXPORT void *
 walters__unescape_js(RubyEnv *rb, const char *cstr, unsigned int size)
 {
-    gh_buf gh = GH_BUF_INIT;
-    return cstring(&gh, houdini_unescape_js(&gh, (const uint8_t *) cstr, size));
+    return houdini(houdini_unescape_js, cstr, size);
 }
