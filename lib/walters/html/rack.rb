b5a5e3df5dc021f9f393754dd198e356d7de8010
Wayne Meissner <wmeissner@gmail.com>
2013-05-02 21:21:42 +1000

    Add Rack::Utils monkey patch

---
+++ lib/walters/html/rack.rb
@@ -0,0 +1,11 @@
+# This file contains code based on the EscapeUtils project.  See the file LICENSE-EscapeUtils.txt for details. 
+require_relative 'html_safety'
+
+module Rack
+  module Utils
+    include ::Walters::HtmlSafety
+
+    alias escape_html walters_escape_html
+    module_function :escape_html
+  end
+end
